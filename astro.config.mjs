b8ed07a738a48eaa4d53377142f48f12ca383588
timon <github@timonlukas.de>
2026-06-24 01:25:36 +0200

    Enable resolve.tsconfigPaths

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    resolve: {
+      tsconfigPaths: true,
+    },
     plugins: [tailwindcss()]
   }
 });
